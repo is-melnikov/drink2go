b95af549bdb1979ed95408c06e450b44688aa9c1
Ivan Melnikov <klubauto.vk@gmail.com>
2022-10-16 10:38:00 +0300

    Updates

---
+++ js/swiper.js
@@ -0,0 +1,1 @@
+const swiper=new Swiper(".swiper",{direction:"horizontal",autoplay:!0,pagination:{el:".swiper__pagination",clickable:!0},navigation:{nextEl:".swiper-button-forward",prevEl:".swiper-button-back"}});
